72de1e7ca30dbb9760f7b4974bca9e1b4ba67c4e
Liqin (Lukas) Zhang <graves_zhang@sjtu.edu.cn>
2021-11-16 16:12:01 -0500

    added mapbox integration to leaflet

--- static/js/map.js
+++ static/js/map.js
@@ -1,23 +1,11 @@
 // world map layer
-var layer = new L.StamenTileLayer("toner");
-var map = new L.Map("mapid", {
-    center: new L.LatLng(40.77, -73.97),
-    zoom: 3
-});
-map.addLayer(layer);
+var mymap = L.map('worldmapid').setView([51.505, -0.09], 13);
 
-
-var nycMarker = L.marker([41.33, -77],
-    {
-        icon: L.icon({
-            iconUrl: '../static/img/city.png',
-            iconSize: [30, 30]
-        })
-    });
-nycMarker.on('click', function (e) {
-    window.location.replace('nyc');
-});
-
-var clusters = L.markerClusterGroup();
-clusters.addLayer(nycMarker);
-map.addLayer(clusters);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ3JhdmVzY243IiwiYSI6ImNrdzFiMmNsbmEyeGwybnFwdzdwdXh3bWgifQ.yrP_vrJ8VRA-1uqTUwkPig'
+}).addTo(mymap);
