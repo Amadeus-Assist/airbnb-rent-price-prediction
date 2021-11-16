fb93328e0cef308dd0ecbc9a132838921bb2f1d4
Liqin (Lukas) Zhang <graves_zhang@sjtu.edu.cn>
2021-11-16 18:10:34 -0500

    cluster almost work

--- static/js/geovisual.js
+++ static/js/geovisual.js
@@ -3,40 +3,36 @@ const lngList = {
 }
 
 // initialize the map
-var layer = new L.StamenTileLayer("toner");
-var map = new L.Map("mapid", {
-    center: new L.LatLng(lngList[city][0], lngList[city][1]),
-    zoom: lngList[city][2]
-});
-map.addLayer(layer);
+var map = L.map('mapid').setView([lngList[city][0], lngList[city][1]], lngList[city][2]);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: '<a href="https://www.mapbox.com/">Mapbox</a> by Liqin Zhang',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ3JhdmVzY243IiwiYSI6ImNrdzFiMmNsbmEyeGwybnFwdzdwdXh3bWgifQ.yrP_vrJ8VRA-1uqTUwkPig'
+}).addTo(map);
 
-// add neighborhood layer
-$.getJSON("../static/data/" + city + "-neighborhoods.geojson", function (hoodData) {
-    L.geoJson(hoodData, {
-        style: function (feature) {
-            var fillColor = "#607d8b";
-            return {color: "#3f51b5", weight: 4, fillColor: fillColor, fillOpacity: .6};
-        },
-        onEachFeature: function (feature, layer) {
-            layer.bindPopup("<strong>" + feature.properties.name + "</strong><br/>")
+
+// add neighborhoods
+$.getJSON("../static/data/" + city + "-neighborhoods.geojson", function (neighborhoods) {
+    L.geoJson(neighborhoods, {
+        style: function () {
+            return {color: "rgba(44,66,194,0.95)", weight: 2, fillColor: "#416272", fillOpacity: .4};
         }
     }).addTo(map);
 });
 
-// add marker
-$.getJSON("../static/data/" + city + "_pre.geojson", function (data) {
-    var roomIcon = L.icon({
-        iconUrl: '../static/img/pin.png',
-        iconSize: [40, 40]
-    });
-    var rooms = L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            var marker = L.marker(latlng, {icon: roomIcon});
-            marker.bindPopup(feature.properties.name + '<br/>' + feature.properties.price);
-            return marker;
+// add houses
+$.getJSON("../static/data/" + city + "_feature.geojson", function (houses) {
+    var rooms = L.geoJson(houses, {
+        pointToLayer: function (feature, lat_lng) {
+            var room = L.marker(lat_lng);
+            room.bindPopup(feature.properties.name + '<br/>' + feature.properties.price);
+            return room;
         }
     });
-    var clusters = L.markerClusterGroup();
-    clusters.addLayer(rooms);
-    map.addLayer(clusters);
+    var markers = L.markerClusterGroup();
+    markers.addLayer(rooms);
+    map.addLayer(markers);
 });
--- static/js/map.js
+++ static/js/map.js
@@ -1,11 +1,16 @@
 // world map layer
-var mymap = L.map('worldmapid').setView([51.505, -0.09], 13);
+var mymap = L.map('worldmapid').setView([40.77, -73.97], 3.5);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    attribution: '<a href="https://www.mapbox.com/">Mapbox</a> by Liqin Zhang',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZ3JhdmVzY243IiwiYSI6ImNrdzFiMmNsbmEyeGwybnFwdzdwdXh3bWgifQ.yrP_vrJ8VRA-1uqTUwkPig'
 }).addTo(mymap);
+
+var nyc_marker = L.marker([40.77, -73.97]).addTo(mymap);
+nyc_marker.on('click', function (e) {
+    window.location.replace('nyc');
+});
