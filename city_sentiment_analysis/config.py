f19dfe8f7055a6aabe509341bc114a89df0ff719
Lisen Dai <lisendai0@gmail.com>
2021-11-17 21:32:30 -0500

    delete credentials

--- city_sentiment_analysis/config.py
+++ city_sentiment_analysis/config.py
@@ -2,13 +2,13 @@ import pandas as pd
 import os 
 
 # Google map API key
-GOOGLE_MAP_API_KEY = 'AIzaSyBK99p_AsbBHJcuCQLOxzCtR1clDl7D-ko'
+GOOGLE_MAP_API_KEY = ''
 
 # Twitter Credentials
-ACCESS_TOKEN = "1133350537204834305-Wft7EpVTfwLgYSfJQdH2gzxiEfsP9o"
-ACCESS_TOKEN_SECRET = "gme5J65D28mA4wdISfPaaKQqmhFZUqJAwXtguiJRbcgn4"
-CONSUMER_KEY = "Ej6ZAEym4PWa5BRtxSepTOaM0"
-CONSUMER_SECRET = "tbBwCbrG4xq5z4In3B6N76WJh6Iox1fMwAnk2sxJb3Silnxp2M"
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
 
 # all the city names
 uscities_table = pd.read_csv("resource/uscities.csv")
