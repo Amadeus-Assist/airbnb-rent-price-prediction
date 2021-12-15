a5c553cf227ffdd9410c2b4874093dd8eb4d0ad3
CharlesK <xk2144@columbia.edu>
2021-12-15 00:37:28 -0500

    update frontend switch tabs and expand

--- static/js/tabswitch.js
+++ static/js/tabswitch.js
@@ -1,48 +1,61 @@
-var switchState = true;
+var switchState = 1;
 
-let shows = []
-var preHousingWhole = document.createElement('div');
-preHousingWhole.id = "prediction-housing-1";
-preHousingWhole.className = "prediction-housing-1";
-shows.push(preHousingWhole);
-var preHousingDetail = document.createElement('div');
-preHousingDetail.id = "prediction-housing-2";
-preHousingDetail.className = "prediction-housing-2";
-shows.push(preHousingDetail);
-var preCovid = document.createElement('div');
-preCovid.id = "prediction-covid";
-preCovid.className = "prediction-covid";
-shows.push(preCovid);
-var housingTrend = document.createElement('div');
-housingTrend.id = "housing-trend";
-housingTrend.className = "housing-trend";
-shows.push(housingTrend);
-var housingAnalysis = document.createElement('div');
-housingAnalysis.id = "housing-analysis";
-housingAnalysis.className = "housing-analysis";
-shows.push(housingAnalysis);
+// let shows = []
+// var preHousingWhole = document.createElement('div');
+// preHousingWhole.id = "prediction-housing-1";
+// preHousingWhole.className = "prediction-housing-1";
+// shows.push(preHousingWhole);
+// var preHousingDetail = document.createElement('div');
+// preHousingDetail.id = "prediction-housing-2";
+// preHousingDetail.className = "prediction-housing-2";
+// shows.push(preHousingDetail);
+// var preCovid = document.createElement('div');
+// preCovid.id = "prediction-covid";
+// preCovid.className = "prediction-covid";
+// shows.push(preCovid);
+// var housingTrend = document.createElement('div');
+// housingTrend.id = "housing-trend";
+// housingTrend.className = "housing-trend";
+// shows.push(housingTrend);
+// var housingAnalysis = document.createElement('div');
+// housingAnalysis.id = "housing-analysis";
+// housingAnalysis.className = "housing-analysis";
+// shows.push(housingAnalysis);
+
+let history = [];
+var shownTab = document.getElementById("shown-tab");
+var invisibleTab = document.getElementById("invisible-tabs");
 
 document.getElementById("history-label").onclick = function(){
-    if(!switchState){
-        switchContents();
-        switchState = !switchState;
+    if(switchState!=1){
+        switchContents("history");
+        switchState = 1;
+    }
+}
+
+document.getElementById("analysis-label").onclick = function(){
+    if(switchState!=2){
+        switchContents("analysis");
+        switchState = 2;
     }
 }
 
 document.getElementById("prediction-label").onclick = function(){
-    if(switchState){
-        switchContents();
-        switchState = !switchState;
+    if(switchState!=3){
+        switchContents("prediction");
+        switchState = 3;
     }
 }
 
-function switchContents(){
-    var container = document.getElementById("chart-container");
-    var alterLength = shows.length;
-    while(container.children.length > 0){
-        shows.push(container.removeChild(container.children[0]));
-    }
-    for(var i = 0;i<alterLength;i++){
-        container.appendChild(shows.shift());
-    }
+function switchContents(id){
+    invisibleTab.appendChild(shownTab.children[0]);
+    shownTab.appendChild(document.getElementById(id));
+    console.log(shownTab.children.length);
+    // var alterLength = shows.length;
+    // while(container.children.length > 0){
+    //     shows.push(container.removeChild(container.children[0]));
+    // }
+    // for(var i = 0;i<alterLength;i++){
+    //     container.appendChild(shows.shift());
+    // }
 }
